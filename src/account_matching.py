0e0448aabe264a4f487b9b15ced2bbd787c7eada
Elizabeth Murnane <elm236@cornell.edu>
2012-06-26 09:20:48 -0400

    handling to filter out twitter accounts with less than min number of tweets

--- src/account_matching.py
+++ src/account_matching.py
@@ -3,17 +3,30 @@ that correspond to the same individual '''
 
 import twitter_util
 
-# Returns objects corresponding to the twitter accounts
-# that match the given usernames, if they exist. Also, filtering
-# out twitter accounts that have tweeted less than the given minimum
+# Returns objects corresponding to the twitter accounts, if 
+# they exist, that have the given usernames. Does not return
+# twitter accounts that have tweeted less than the given minimum
 # number of tweets.
 def find_twitter_matches(usernames, min_tweets):
     print "Looking up Twitter accounts..."
+    # lookup..
     userinfos = twitter_util.batch_userlookup(usernames)
-    return userinfos
+    # filter out..
+    matches = []
+    for account in userinfos:
+        try:
+            num_tweets = account['statuses_count']
+            if num_tweets >= min_tweets:
+                matches.append(account)
+        except:
+            continue
+    return matches
     
 # Calculates and returns the probability that the
 # given wikipedia editor and the given twitter account
 # belong to the same person.
+''' TODO '''
 def probability_same(wikipedia_user, twitter_user):
     print "calculating prob"
+    
+
