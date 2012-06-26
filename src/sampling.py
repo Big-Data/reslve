4f60a258e791852500110021ccfb947cc4887eec
Elizabeth Murnane <elm236@cornell.edu>
2012-06-26 11:30:13 -0400

    Scripts for building a sample: - fetching active wiki editors, - fetching matching twitter accounts, - fetching the tweets of those twitter accounts, - fetching named entities + their candidate resources in those tweets, - fetching categories of articles edited by the users (to be used for disambiguation of candidates)

--- src/sampling.py
+++ src/sampling.py
@@ -2,8 +2,12 @@
 Finds a sample of users that are both wikipedia editors and twitter 
 users. Writes their edited articles and tweets to file.
 '''
+import User_Categories
 import account_matching
+import named_entity_finder
 import pickle
+import pprint
+import string
 import twitter_util
 import wiki_fetch_util
 
@@ -13,7 +17,7 @@ def fetch_wiki_editors():
     editors = wiki_fetch_util.fetch_n_active_editors(1500) 
     
     # write to file
-    editors_file = open('editors.pkl', 'wb')
+    editors_file = open('sample_dump/editors.pkl', 'wb')
     pickle.dump(editors, editors_file)
     editors_file.close()
     print "Retrieved "+str(len(editors))+" active wikipedia editors"
@@ -21,7 +25,7 @@ def fetch_wiki_editors():
 def fetch_matching_tweeters():
     print "Matching wikipedia editors to twitter accounts..."
     # load active wiki editors
-    editors_file = open('editors.pkl', 'rb')
+    editors_file = open('sample_dump/editors.pkl', 'rb')
     editors = pickle.load(editors_file)
     
     # find twitter accounts belonging to the same users
@@ -30,9 +34,68 @@ def fetch_matching_tweeters():
     print "Found "+str(len(twitter_accounts))+" matching twitter accounts"
     
     # write to file
-    twitter_accounts_file = open('twitter_accounts.pkl', 'wb')
+    twitter_accounts_file = open('sample_dump/twitter_accounts.pkl', 'wb')
     pickle.dump(twitter_accounts, twitter_accounts_file)
     twitter_accounts_file.close()
+    
+def fetch_tweet_named_entities():
+    print "Detecting surface forms in tweets and identifying candidate resources..."
+    tweets_file = open('sample_dump/tweets.pkl', 'rb')
+    tweets = pickle.load(tweets_file)
+    tweet_candidates = {}
+    #count = 0
+    for user in tweets:
+        #if count > 10: 
+        #    break
+        user_tweets = tweets[user]
+        for tweet in user_tweets:
+            print "tweet! "+tweet
+            try:
+                named_entities = named_entity_finder.find_named_entities(tweet)
+                pprint.pprint(named_entities)
+                surface_forms = named_entities['annotation']['surfaceForm']
+                tweet_candidates[tweet] = surface_forms
+            except:
+                continue
+        #count = count + 1
+        
+    # write to file
+    tweet_candidates_file = open('sample_dump/tweet_candidates.pkl', 'wb')
+    pickle.dump(tweet_candidates, tweet_candidates_file)
+    tweet_candidates_file.close()
+    
+
+def get_user_mapping(filename):
+    userfile = open(filename, 'rb')
+    mapping = pickle.load(userfile)
+    # username may be capitalized for one account and not other, so ignore case
+    mapping = dict(zip(map(string.lower,mapping.keys()),mapping.values()))
+    return mapping
+def get_editors_mapping():
+    return get_user_mapping('sample_dump/editors.pkl')
+def get_twitter_accounts_mapping():
+    return get_user_mapping('sample_dump/tweets.pkl')
+    
+def build_user_models():
+    print "Creating user's category mapping based on wikipedia edits..."
+    # load users with active wiki + twitter accounts
+    editors = get_editors_mapping()
+    twitter_users = get_twitter_accounts_mapping()
+    user_models = {}
+    for username in twitter_users.keys():
+        # get the categories of user's edited pages and put them in a mapping 
+        # from category -> number of edits made to pages with that category
+        edited_pages = editors[username]
+        user_interests = User_Categories.User_Categories(username)
+        for edited_page_id in edited_pages:
+            categories = wiki_fetch_util.fetch_categories_of_id(edited_page_id)
+            user_interests.add_categories(categories)
+        user_models[username] = user_interests
+        
+    # write to file
+    interests_file = open('sample_dump/interests_models.pkl', 'wb')
+    pickle.dump(user_models, interests_file)
+    interests_file.close()
     
 # fetch wiki editors
 #fetch_wiki_editors()
@@ -42,3 +105,10 @@ def fetch_matching_tweeters():
 
 # fetch the tweets of those twitter accounts
 # twitter_util.fetch_tweets()
+
+# fetch named entities of those tweets
+#fetch_tweet_named_entities()
+
+# build user interest model for those users, which we will use
+# to find candidate resources for ambiguous entities in the tweets
+#build_user_models()
