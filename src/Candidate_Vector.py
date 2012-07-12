827dcb8890ddfe470a9ddec7722cad14b1dc3f35
Elizabeth Murnane <elm236@cornell.edu>
2012-07-12 12:58:26 -0400

    Actually storing the candidate ID, not the candidate URI

--- src/Candidate_Vector.py
+++ src/Candidate_Vector.py
@@ -1,12 +1,12 @@
 class Candidate_Vector:
     
-    def __init__(self, candidate_URI, weighted_category_vector):
-        self.__candidate_URI__ = candidate_URI
+    def __init__(self, candidateID, weighted_category_vector):
+        self.__candidate_ID__ = candidateID
         self.__weighted_category_vector__ = weighted_category_vector
 
     # Returns the wikipedia page URI of this candidate
-    def get_candidate_URI(self):
-        return self.__candidate_URI__
+    def get_candidate_ID(self):
+        return self.__candidate_ID__
     
     def get_weighted_vector(self):
         ''' Returns the weighted vector of category -> weight '''
