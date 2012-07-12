d421b498b7a2e15b0201e359586f8a3bba74d1b0
Elizabeth Murnane <elm236@cornell.edu>
2012-07-12 12:55:05 -0400

    New methods to access the source article of a category hierarchy as well as the distances from that source article of any category in the hierarchy.

--- src/Category_Hierarchy.py
+++ src/Category_Hierarchy.py
@@ -8,8 +8,24 @@ class Category_Hierarchy:
         @param source_article_id: The id of the article from which 
             this category hierarchy originates
         @param category_to_distance: A mapping from category id -> number of 
-            steps away  from the source article that category is positioned 
+            steps away from the source article that category is positioned 
             in this category hierarchy graph
         '''
         self.__source_article_id__ = source_article_id
         self.__category_to_distance__ = category_to_distance
+        
+    def get_source_article_id(self):
+        return self.__source_article_id__
+    
+    def get_category_distance(self, category):
+        ''' For a given category, returns the number of steps away from the 
+        source article that category is positioned in this hierarchy '''
+        if not category in self.__category_to_distance__:
+            return -1
+        return self.__category_to_distance__[category]
+        
+    def get_category_to_distance(self):
+        ''' Returns a mapping from category id to the number of steps 
+        away from the source article that the category is positioned 
+        in this category hierarchy graph '''
+        return self.__category_to_distance__
