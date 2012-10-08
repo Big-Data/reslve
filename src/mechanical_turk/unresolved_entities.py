89bd9fd9798d67a27e4a10099f685f27223cee99
Elizabeth Murnane <elm236@cornell.edu>
2012-10-07 20:43:32 -0400

    making sure the entity presented to a turker looks the same as it appears in the short text

--- src/mechanical_turk/unresolved_entities.py
+++ src/mechanical_turk/unresolved_entities.py
@@ -26,6 +26,12 @@ def make_entities_csv_for_turk():
         
         # Tell turker to look at entity and choose correct candidate
         entity_str = surface_form_obj.entity_str
+        
+        # make sure the entity presented to a Turker looks the same as
+        # it appears in the short text (ie with the same capitalization)
+        if not entity_str in dirty_shorttext:
+            entity_str = __match_appearance__(entity_str, dirty_shorttext)
+        
         print "From the below set of choices, please select the correct intended meaning of the term \""\
         +str(entity_str)+"\""
         
@@ -48,5 +54,12 @@ def make_entities_csv_for_turk():
             
     # dump to csv
     csv_util.write_to_spreadsheet(__entities_to_judge_csv_path__, rows)
+    
+def __match_appearance__(entity_str, shorttext_str):
+    ''' Seems that Wikipedia Miner sometimes returns an entity 
+    capitalized even if it is lowercase in the original string '''
+    if entity_str.lower() in shorttext_str:
+        return entity_str.lower()
+    return entity_str
     
 make_entities_csv_for_turk()
