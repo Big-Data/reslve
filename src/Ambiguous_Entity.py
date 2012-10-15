aec379d65bb014f0640d499759a5fba7ae583f68
Elizabeth Murnane <elm236@cornell.edu>
2012-10-15 00:16:44 -0400

    Using titles as unique ids of named entities and candidates rather than article IDs since some services don't store these IDs (and querying to get them inconsistently gives unicode and int)

--- src/Ambiguous_Entity.py
+++ src/Ambiguous_Entity.py
@@ -2,30 +2,30 @@
 Represents an ambiguous surface form detected by wikiminer in a short text on the site
 '''
 from CONSTANT_VARIABLES import VALID_RDF_TYPES
-from Mixins import EqualityMixin
 from wikipedia import wikipedia_api_util
 import simplejson
 import text_util
 import urllib2
 
 class NamedEntity:
-    ''' Represents a named entity detected in a short text '''
+    ''' Represents a named entity detected in a short text.
+    We use the entity's surface form as its unique identifier. That means that
+    for short texts containing multiple entities with the same surface form, 
+    we'll treat all occurrences as a single entity. '''
     
-    def __init__(self, entity_str, entity_offset,
+    def __init__(self, surface_form,
                  shorttext_id, shorttext_str,
                  candidate_res_objs, 
                  username, site):
         '''
-        @param entity_str: the surface form of the named entity this object represents
-        @param entity_offset: the offset of this named entity within the containing short text
+        @param surface_form: the surface form of the named entity this object represents
         @param shorttext_id: the ID of the short text that contains this entity
         @param shorttext_str: the string of the short text that contains this entity
         @param candidate_res_objs: a list of Candidate_Resource objects that this named entity could refer to
         @param username: the username who authored the short text containing this entity
         @param site: the site on which the short text containing this entity was posted
         '''
-        self.entity_str = entity_str
-        self.entity_offset = entity_offset
+        self.surface_form = surface_form
         self.shorttext_id = shorttext_id
         self.shorttext_str = shorttext_str
         self.candidate_res_objs = candidate_res_objs
@@ -50,10 +50,10 @@ class NamedEntity:
         #print "Candidates of "+str(self.entity_str)+" are: "+\
         #str([candidate['dbpedia_uri'] for candidate in self.candidates])
         valid_candidate_resources = []
-        for candidate in self.candidates:
+        for candidate in self.candidate_res_objs:
             try:
-                candidate_title = candidate['title'].replace(' ', '_')
-                dbpedia_uri = candidate['dbpedia_uri']
+                candidate_title = candidate.title.replace(' ', '_')
+                dbpedia_uri = candidate.dbpedia_URI
             
                 response = urllib2.urlopen("http://dbpedia.org/data/"+candidate_title+".json").read()
                 response = simplejson.loads(response.decode('utf-8'))
@@ -79,39 +79,22 @@ class NamedEntity:
         # none of its associated types are what we consider valid
         return False
     
-class CandidateResource(EqualityMixin):
+class CandidateResource:
     ''' Represents a candidate resource that an ambiguous named entity may refer to.
     Depending on whether the entity was detected by DBPedia Spotlight, Wikipedia Miner, or
     both, this object stores those services' scores that this candidate is the correct one ''' 
     
-    SCORE_NOT_AVAILBLE_VALUE = 'Score_Unavailable'
+    SCORE_NOT_AVAILBLE = 'Score_Unavailable'
     
-    def __init__(self, title, dbpedia_URI, scores={}, wikipedia_pageID=None):
+    def __init__(self, title, dbpedia_URI, 
+                 wikiminer_score=SCORE_NOT_AVAILBLE, dbpedia_score=SCORE_NOT_AVAILBLE):
         '''
         @param title: The title of this candidate's wikipedia/dbpedia page (same thing) 
         @param dbpedia_URI: The URI of this candidate's dbpedia resource page
-        @param scores: The score(s) for this candidate from DBPedia Spotlight, 
-                       Wikipedia Miner, or both. Will be a mapping from 
-                       DBPEDIA_SPOTLIGHT_SCORE_KEY->int, WIKIPEDIA_MINER_SCORE_KEY->int
-        @param wikipedia_pageID: The ID of this candidate's wikipedia article page
+        @param wikiminer_score: The score for this candidate from Wikipedia Miner 
+        @param dbpedia_score: The score for this candidate from DBPedia Spotlight 
         '''
         self.title = title
         self.dbpedia_URI = dbpedia_URI
-            
-        # Make sure each type of service has a key for its score,
-        # evven if that service did not detect this entity/candidate
-        self.scores = scores
-        if not self.DBPEDIA_SPOTLIGHT_SCORE_KEY in scores:
-            scores[self.DBPEDIA_SPOTLIGHT_SCORE_KEY] = self.SCORE_NOT_AVAILBLE
-        if not self.WIKIPEDIA_MINER_SCORE_KEY in scores:
-            scores[self.WIKIPEDIA_MINER_SCORE_KEY] = self.NOT_AVAILBLE
-            
-        # If page ID not supplied, determine it from the page title
-        self.wikipedia_pageID = wikipedia_pageID
-        if self.wikipedia_pageID is None:
-            self.wikipedia_pageID = wikipedia_api_util.query_page_id(title)
-                
-        def is_equal(self, other):
-            print "self.dict??"
-            print self.dict
-            
+        self.wikiminer_score = wikiminer_score
+        self.dbpedia_score = dbpedia_score
