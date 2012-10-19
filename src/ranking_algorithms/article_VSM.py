841f0543b82c03e1724e4b549498acbb61ce6d2d
Elizabeth Murnane <elm236@cornell.edu>
2012-10-19 15:25:49 -0400

    Two new algorithms based on article attributes. Specifically: one algorithm with a sim based on overlap between words in candidate article's title with titles of articles the user has edited; and second algorithm with sim measure based on overlap of id of candidate article with user's edited articles, ie non-zero sim only if user has edited a candidate article.

--- src/ranking_algorithms/article_VSM.py
+++ src/ranking_algorithms/article_VSM.py
@@ -6,7 +6,7 @@ import text_util
 #def rank_articleBOW_VSM_numedits(candidates, username):
 '''
 
-class ArticleBOW_VSM(VSM_Algorithm):
+class Article_ContentBOW_VSM(VSM_Algorithm):
     ''' An algorithm with a similarity measure between the BOW of the candidate 
     resource's article page and the BOW of the user's edited articles' pages '''
     
@@ -20,3 +20,36 @@ class ArticleBOW_VSM(VSM_Algorithm):
         article_text = wikipedia_api_util.query_page_content_text(article_title) # get the text of the article's page
         cleaned_tokens = text_util.get_clean_BOW_doc(article_text) # clean the article text 
         return cleaned_tokens
+
+    
+class Article_ID_VSM(VSM_Algorithm):
+    ''' An algorithm with a similarity measure based on the
+    candidate article's ID and the user's edited articles' IDs matching.
+    In other words, this algorithm will only result in non-zero
+    similarity measures for candidates that correspond to articles
+    the user has edited. '''
+    
+    def __init__(self):
+        VSM_Algorithm.__init__(self, "article ID", "articleID")
+    
+    def get_article_representation(self, article_title):
+        ''' Returns a list containing the id of this article
+        @return: a list of numbers '''
+        article_model = [wikipedia_api_util.query_page_id(article_title)]
+        return article_model
+    
+class Article_TitleBOW_VSM(VSM_Algorithm):
+    ''' An algorithm with a similarity measure based on the candidate's 
+    article title BOW and the user's articles' titles BOW. In other words,
+    this algorithm measures how many words in a candidate article's title
+    overlap with words in titles of articles the user has edited. '''
+    
+    def __init__(self):
+        VSM_Algorithm.__init__(self, "article's title BOW", "articleTitleBOW")
+    
+    def get_article_representation(self, article_title):
+        ''' Returns a bag of words build from the title of the given article.
+        @return: a list of tokens '''
+        article_title_str = article_title.replace('_', ' ')
+        cleaned_title = text_util.get_clean_BOW_doc(article_title_str)
+        return cleaned_title
