3fbdbbc3f5db4f7bf86e463dd0102dfc9f533152
Elizabeth Murnane <elm236@cornell.edu>
2012-10-28 21:23:47 -0400

    Handling to extract turker judgments about candidate resources

--- src/mechanical_turk/unresolved_entities_task.py
+++ src/mechanical_turk/unresolved_entities_task.py
@@ -1,9 +1,12 @@
-from dataset_generation import entity_dataset_mgr, csv_util
+from dataset_generation import entity_dataset_mgr, csv_util, pkl_util
 from short_text_sources import short_text_websites
 from wikipedia import wikipedia_api_util
 import random
 
 __entities_to_judge_csv_path__ = 'entities-for-turk.csv'
+
+__entities_results_csv_path__ = '/Users/elizabethmurnane/git/reslve/src/mechanical_turk/mturk_entity_disambiguation_results_complete.csv'
+__entity_judgments_cache_path__ = '/Users/elizabethmurnane/git/reslve/data/pickles/entity_judgments_cache.pkl'
 
 def make_tweet_entities_csv_for_turk():
     twitter_site = short_text_websites.get_twitter_site()
@@ -80,5 +83,83 @@ def __match_appearance__(surface_form, shorttext_str):
     if surface_form.lower() in shorttext_str:
         return surface_form.lower()
     return surface_form
+
+def get_entity_judgements():
+    output_str = "Candidate entities judged by Mechanical Turkers..."
+    entity_judgments = pkl_util.load_pickle(output_str, 
+                                            __entity_judgments_cache_path__)
+    if False and entity_judgments is not None:
+        return entity_judgments
+    return analyze_entities_judgments(output_str)
+
+def analyze_entities_judgments(output_str):
+    ''' Handles updating the entities 
+    spreadsheet given new Mechanical Turker evaluations '''
+    print "Updating entities spreadsheet with Mechanical Turker "+\
+    "judgments about correct candidate resources..."
     
-make_tweet_entities_csv_for_turk()
+    judgments = {} # maps { entity ID -> { candidate link -> (num times judged correct, num times judged wrong) }}
+    row_num = 0
+    rows_plus_headers = csv_util.query_csv_for_rows(__entities_results_csv_path__, False)
+    for row in rows_plus_headers:
+        try:
+            if row_num==0: # row 0 is header
+                entity_id_col = row.index('Input.entity_id')
+                candidate_link_col = row.index('Input.candidate_link') 
+                
+                #turkerID_col = row.index('WorkerId')
+                answer_col = row.index('Answer.Q1')
+            else:
+                judged_entity_id = row[entity_id_col]
+                
+                if judged_entity_id in judgments:
+                    selected_candidates = judgments[judged_entity_id]
+                else:
+                    selected_candidates = {}
+                    
+                selected_candidate = row[candidate_link_col]
+                if selected_candidate in selected_candidates:
+                    (num_true, num_false) = selected_candidates[selected_candidate]
+                else:
+                    (num_true, num_false) = (0,0)
+                    
+                judgment = row[answer_col]
+                if judgment=='true':
+                    num_true = num_true+1
+                else:
+                    num_false = num_false+1
+                selected_candidates[selected_candidate] = (num_true, num_false)
+                judgments[judged_entity_id] = selected_candidates
+                    
+            row_num = row_num+1    
+        except:
+            continue # just ignore a problematic row    
+        
+    # Get the candidates that workers agreed upon for each entity.
+    # For now, ignoring entities that all workers could not select the same candidate for
+    user_selected_candidates = {}
+    for entity_id in judgments:
+        agreed_candidates = []
+        
+        candidate_judgments = judgments[entity_id]
+        for candidate_link in candidate_judgments:
+            (num_true, num_false) = candidate_judgments[candidate_link]
+            if num_true > num_false:
+                # TODO need to figure out threshold for agreement. require 
+                # turkers to unanimously select a candidate as correct?
+                agreed_candidates.append(candidate_link)
+        user_selected_candidates[entity_id] = agreed_candidates
+    
+    print "Cached a total of "+str(len(user_selected_candidates))+" Turker judgements about ambiguous entities"
+    pkl_util.write_pickle(output_str, user_selected_candidates, __entity_judgments_cache_path__)
+    return user_selected_candidates
+
+''' 
+prompt_make_or_extract = raw_input("Make entities task for Turkers (A) or analyze completed task (B)? ")
+if 'A'==prompt_make_or_extract or 'a'==prompt_make_or_extract:
+    make_tweet_entities_csv_for_turk()
+elif 'B'==prompt_make_or_extract or 'b'==prompt_make_or_extract:
+    get_entity_judgements()
+else:
+    print "Unrecognized input, exiting."   
+'''
