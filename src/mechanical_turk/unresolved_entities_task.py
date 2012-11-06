bc002cbe9abd5597eadb8874a99fccc1c3f2d5cb
Elizabeth Murnane <elm236@cornell.edu>
2012-11-06 10:20:37 -0500

    just getting 50 entities per user (enough for statistical significance) to limit the dataset given our resources for turk

--- src/mechanical_turk/unresolved_entities_task.py
+++ src/mechanical_turk/unresolved_entities_task.py
@@ -4,7 +4,7 @@ from short_text_sources import short_text_websites
 from wikipedia import wikipedia_api_util
 import random
 
-__entities_to_judge_csv_path__ = '/Users/elizabethmurnane/git/reslve/data/mechanical_turk/entities-for-turk2.csv'
+__entities_to_judge_csv_path__ = '/Users/elizabethmurnane/git/reslve/data/mechanical_turk/entities-for-turk_Twitter2.csv'
 __entities_results_csv_path__ = '/Users/elizabethmurnane/git/reslve/data/mechanical_turk/mturk_entity_disambiguation_results_complete.csv'
 
 def make_tweet_entities_csv_for_turk():
@@ -31,49 +31,90 @@ def make_tweet_entities_csv_for_turk():
         except:
             continue # just ignore a problematic row      
         
+    # Determine what entity+candidate tasks we actually want to write to a spreadsheet 
+    # and send to mturk since we don't have resources for unlimited mturk tasks
+    tasks = {} # NamedEntity object -> candidate judgment tasks we actually want performed
+    user_entities = defaultdict(list) # username -> [NamedEntity obj]
+    done_shorttexts = [] # list of shorttext id
+    random.shuffle(entities_to_evaluate) # so we get a random subset of a user's entities
+    for ne_obj in entities_to_evaluate:
+        
+        # "40 nouns usually enough to establish statistically significant 
+        # differences between WSD algorithms" (Santamaria et al., 2010)
+        username = ne_obj.username
+        if len(user_entities[username]) > 50:
+            continue # have enough entities for this user
+        
+        # limiting our dataset to one named entity per short text
+        shorttext_id = ne_obj.shorttext_id
+        if shorttext_id in done_shorttexts:
+            continue
+        
+        # no need to create tasks for candidates we already have annotator judgments for
+        entity_id = ne_obj.get_entity_id()
+        candidate_URLs = ne_obj.get_candidate_wikiURLs()
+        valid_candidate_tasks = []
+        for candidate_URL in candidate_URLs:
+            if ((entity_id, candidate_URL) in already_judged):
+                continue
+            valid_candidate_tasks.append(candidate_URL)
+        if len(valid_candidate_tasks)==0:
+            continue # already have annotator judgments for all of this entity's candidates
+        if len(candidate_URLs)+len(valid_candidate_tasks) < 2:
+            # this would be a non-ambiguous entity, and we should never reach this 
+            # point because such entities should have been filtered out by now
+            raise
+        tasks[entity_id] = valid_candidate_tasks
+        user_entities[username].append(ne_obj)
+        done_shorttexts.append(shorttext_id)
+            
+    # put valid entities + candidates in the spreadsheet until reach our limit of tasks
+    task_max = 1400    
+    
     rows = []
     headers = ['entity_id', 'short_text', 'ambiguous_entity', 'candidate_link']
     rows.append(headers)
     
-    # put valid entities + candidates in the spreadsheet
-    progress = 0
-    for ne_obj in entities_to_evaluate:
+    for username in user_entities:
         
-        progress = progress+1
-        if progress%50==0:
-            print str(progress)+" out of "+str(len(entities_to_evaluate))
-        '''    keep_adding = raw_input("Added "+str(len(rows)-1)+" to spreadsheet out of "+str(len(entities_to_evaluate))+". Continue adding more? (Y/N)")
-            if 'Y'!=keep_adding and 'y'!=keep_adding:
-                break
-        '''
-            
-        # shuffle candidates so that they don't appear
-        # in wikiminer's ranking order and bias the turker
-        candidate_URLs = ne_obj.get_candidate_wikiURLs()
-        random.shuffle(candidate_URLs)
-        choices = candidate_URLs[:] # copy (list slicing)
-            
-        # make sure the entity presented to a Turker looks the same as
-        # it appears in the short text (ie with the same capitalization)
-        original_shorttext = ne_obj.shorttext_str.decode('latin-1')
-        surface_form = ne_obj.surface_form
-        if not surface_form in original_shorttext:
-            surface_form = __match_appearance__(surface_form, original_shorttext)
+        # add users until reach our limit on the number of tasks we can afford, 
+        # but break at this point in the loop rather than in the inner loop to
+        # ensure that we do have at least 50 entities per user (even if this
+        # means we go over our task limit a little in order to reach that amount)
+        if len(rows) > task_max:
+            break
         
-        entity_id = ne_obj.get_entity_id()
-        for choice in choices:
-            if ((entity_id, choice) in already_judged):
-                continue
-            
-            # make a separate row for each candidate link 
-            # rather than putting all links in a single cell
-            row = [entity_id, original_shorttext, surface_form, choice]
-            rows.append(row)
+        # bypass users who haven't written the minimum number of valid entities
+        # required to establish statistical significance between the algorithms
+        if len(user_entities[username]) < 50:
+            continue
         
-        if len(rows)%50==0:
-            # write the rows every once in a while in case we reach an error
-            print "Updating spreadsheet..."+str(len(rows))
-            csv_util.write_to_spreadsheet(__entities_to_judge_csv_path__, rows)
+        # should be 50 NamedEntity objects per user, and we'll make tasks for their candidates
+        for ne_obj in user_entities[username]:
+            entity_id = ne_obj.get_entity_id()
+        
+            # make sure the entity presented to a Turker looks the same as
+            # it appears in the short text (ie with the same capitalization)
+            original_shorttext = ne_obj.shorttext_str.decode('latin-1')
+            surface_form = ne_obj.surface_form
+            if not surface_form in original_shorttext:
+                surface_form = __match_appearance__(surface_form, original_shorttext)
+            
+            # shuffle candidates so that they don't appear
+            # in wikiminer's/dbpedia's ranking order and bias the turker
+            candidate_URLs = tasks[entity_id]
+            random.shuffle(candidate_URLs)
+            choices = candidate_URLs[:] # copy (list slicing)
+            for choice in choices:
+                # make a separate row for each candidate link 
+                # rather than putting all links in a single cell
+                row = [entity_id, original_shorttext, surface_form, choice]
+                rows.append(row)
+            
+            if len(rows)%50==0:
+                # write the rows every once in a while in case we reach an error
+                print "Updating spreadsheet..."+str(len(rows))
+                csv_util.write_to_spreadsheet(__entities_to_judge_csv_path__, rows)
         
     # dump to csv
     csv_util.write_to_spreadsheet(__entities_to_judge_csv_path__, rows)
